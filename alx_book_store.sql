4d6491374187b332e750376d21043e217c851457
unyime <unyimeusen01@gmail.com>
2025-02-02 22:36:24 +0100

    correction

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,23 +1,23 @@
-CREATE DATABASE ALX_BOOK_STORE;
-USE ALX_BOOK_STORE;
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
 
-CREATE TABLE AUTHORS (
+CREATE TABLE Authors (
     author_id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL,
     PRIMARY KEY (author_id)
 );
 
-CREATE TABLE BOOKS (
+CREATE TABLE Books (
     book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
     PRIMARY KEY (book_id),
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id INT NOT NULL AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -25,20 +25,20 @@ CREATE TABLE CUSTOMERS (
     PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT NOT NULL AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE,
     PRIMARY KEY (order_id),
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_Details (
     orderdetailid INT NOT NULL AUTO_INCREMENT,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     PRIMARY KEY (orderdetailid),
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
